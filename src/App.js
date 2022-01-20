70bd090b3d97a6586d0602924d0475e3926ddc13
hmao1 <35634135+hmao1@users.noreply.github.com>
2022-01-19 22:10:16 -0500

    add signup logic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import Note from "./components/Note";
 import Count from "./components/Count";
 import Footer from "./components/Footer";
 
+//bootstrap
+import SignUp from "./components/SignUp";
+import { Container } from "react-bootstrap"
+// import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./components/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/ForgotPassword"
+
+
+
 function App(props) {
   const [notes, setNotes] = useState([]);
 
@@ -23,6 +34,28 @@ function App(props) {
   }
   return (
     <div className="App">
+
+<Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "650px" }}>
+        <Router>
+         
+            <Switch>
+
+              <Route path="/signup" component={SignUp} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          
+        </Router>
+      </div>
+    </Container>
+
+
+
+
       <Header />
       <Count
         count={
